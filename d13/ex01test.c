6da29c3625d2df4a7e37810949b552e5d4692399
Willehard Korander <willehard@gmail.com>
2019-07-27 11:06:16 +0300

    Fixes

--- d13/ex01test.c
+++ d13/ex01test.c
@@ -13,6 +13,21 @@
 #include <stdio.h>
 #include "../ft_btree_all.h"
 
+#define COUNT 10
+
+void    infix_traversal(t_btree *node, int ws)
+{
+	if (node == NULL)
+		return ;
+	ws += COUNT;
+	infix_traversal(node->right, ws);
+	printf("\n");
+	for (int i = COUNT; i < ws; i++)
+		printf(" ");
+	printf("%s\n", node->item);
+	infix_traversal(node->left, ws);
+}
+
 void print_item(char *item)
 {
 	printf("%s", item);
@@ -29,6 +44,10 @@ int	main(void)
 	
 	root->right = btree_create_node("3");
 	root->right->right = btree_create_node("4");
+
+	infix_traversal(root, ' ');
+
+	printf("Should be: 210134\n");
 
 	btree_apply_prefix(root, (void(*)(void*))print_item);
 }
